7d239120d279c09e7284158da89f14dd6f18d6e1
Sungjun Kim <sungjun5.kim@samsung.net>
2019-03-12 15:54:54 +0900

    init knn, linear learner added

--- function/python/brightics/function/sagemaker/__init__.py
+++ function/python/brightics/function/sagemaker/__init__.py
@@ -2,3 +2,7 @@ from .kmeans import kmeans_train
 from .kmeans import kmeans_predict
 from .factorization_machines import factorization_machines_train
 from .factorization_machines import factorization_machines_predict
+from .knn import knn_train
+from .knn import knn_predict
+from .linear_learner import linear_learner_train
+from .linear_learner import linear_learner_predict
